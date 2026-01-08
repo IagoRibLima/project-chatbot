e584ac886e86d73e40978a171152f734be92bb34
IagoRibLima <iagolima40028922@gmail.com>
2026-01-08 12:15:25 -0300

    base do chatbot

---
+++ Python/chatbot.py
@@ -0,0 +1,19 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_MODEL = "gemini-2.5-flash"
+
+client = genai.Client(api_key=API_KEY)
+chat = client.chats.create(model=GEMINI_MODEL)
+
+print(chat.get_history())
+
+prompt = input("Esperando prompt: ")
+
+while prompt != "fim":
+    response = chat.send_message(prompt)
+    print(response.text)
+    prompt = input("Esperando prompt: ")
